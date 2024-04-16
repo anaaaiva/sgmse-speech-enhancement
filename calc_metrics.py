f0aafcefd7aa2689cbe3fd5fa67bc5033c55edf3
Anastasia Iva <ivawillow.a@gmail.com>
2024-04-16 12:23:47 +0300

    modified calc_metrics.py

--- calc_metrics.py
+++ calc_metrics.py
@@ -1,20 +1,31 @@
-from os.path import join 
-from glob import glob
 from argparse import ArgumentParser
-from soundfile import read
-from tqdm import tqdm
-from pesq import pesq
-import pandas as pd
+from glob import glob
+from os.path import join
 
-from pystoi import stoi
+import pandas as pd
+from soundfile import read
+from torchmetrics.audio import (
+    PerceptualEvaluationSpeechQuality,
+    ShortTimeObjectiveIntelligibility,
+)
+from tqdm import tqdm
 
 from sgmse.util.other import energy_ratios, mean_std
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--test_dir", type=str, required=True, help='Directory containing the original test data (must have subdirectories clean/ and noisy/)')
-    parser.add_argument("--enhanced_dir", type=str, required=True, help='Directory containing the enhanced data')
+    parser.add_argument(
+        "--test_dir",
+        type=str,
+        required=True,
+        help="Directory containing the original test data (must have subdirectories clean/ and noisy/)",
+    )
+    parser.add_argument(
+        "--enhanced_dir",
+        type=str,
+        required=True,
+        help="Directory containing the enhanced data",
+    )
     args = parser.parse_args()
 
     test_dir = args.test_dir
@@ -22,43 +33,40 @@ if __name__ == '__main__':
     noisy_dir = join(test_dir, "noisy/")
     enhanced_dir = args.enhanced_dir
 
-    data = {"filename": [], "pesq": [], "estoi": [], "si_sdr": [], "si_sir": [],  "si_sar": []}
+    data = {
+        "filename": [],
+        "pesq": [],
+        "estoi": [],
+        "si_sdr": [],
+        "si_sir": [],
+        "si_sar": [],
+    }
     sr = 16000
 
     # Evaluate standard metrics
-    noisy_files = sorted(glob('{}/*.wav'.format(noisy_dir)))
+    noisy_files = sorted(glob("{}/*.wav".format(noisy_dir)))
     for noisy_file in tqdm(noisy_files):
-        filename = noisy_file.split('/')[-1]
+        filename = noisy_file.split("/")[-1]
         x, _ = read(join(clean_dir, filename))
         y, _ = read(noisy_file)
-        n = y - x 
+        n = y - x
         x_method, _ = read(join(enhanced_dir, filename))
 
+        wb_pesq = PerceptualEvaluationSpeechQuality(sr, "wb")
+        stoi = ShortTimeObjectiveIntelligibility(sr, True)
+
         data["filename"].append(filename)
-        data["pesq"].append(pesq(sr, x, x_method, 'wb'))
-        data["estoi"].append(stoi(x, x_method, sr, extended=True))
+        data["pesq"].append(wb_pesq(x, x_method))
+        data["estoi"].append(stoi(x, x_method))
         data["si_sdr"].append(energy_ratios(x_method, x, n)[0])
         data["si_sir"].append(energy_ratios(x_method, x, n)[1])
         data["si_sar"].append(energy_ratios(x_method, x, n)[2])
 
-    # Save results as DataFrame    
+    # Save results as DataFrame
     df = pd.DataFrame(data)
-
-    # POLQA evaluation  -  requires POLQA license and server, uncomment at your own peril.
-    # This is batch processed for speed reasons and thus runs outside the for loop.
-    # if not basic:
-    #     clean_files = sorted(glob('{}/*.wav'.format(clean_dir)))
-    #     enhanced_files = sorted(glob('{}/*.wav'.format(enhanced_dir)))
-    #     clean_audios = [read(clean_file)[0] for clean_file in clean_files]
-    #     enhanced_audios = [read(enhanced_file)[0] for enhanced_file in enhanced_files]
-    #     polqa_vals = polqa(clean_audios, enhanced_audios, 16000, save_to=None)
-    #     polqa_vals = [val[1] for val in polqa_vals]
-    #     # Add POLQA column to DataFrame
-    #     df['polqa'] = polqa_vals
 
     # Print results
     print(enhanced_dir)
-    #print("POLQA: {:.2f} ± {:.2f}".format(*mean_std(df["polqa"].to_numpy())))
     print("PESQ: {:.2f} ± {:.2f}".format(*mean_std(df["pesq"].to_numpy())))
     print("ESTOI: {:.2f} ± {:.2f}".format(*mean_std(df["estoi"].to_numpy())))
     print("SI-SDR: {:.1f} ± {:.1f}".format(*mean_std(df["si_sdr"].to_numpy())))
