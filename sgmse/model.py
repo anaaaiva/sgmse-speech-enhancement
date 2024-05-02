42ff70818058edc278b839ac2793e450668570f1
Anastasia Iva <ivawillow.a@gmail.com>
2024-05-02 18:57:05 +0000

    modify model.py

--- sgmse/model.py
+++ sgmse/model.py
@@ -4,13 +4,12 @@ from math import ceil
 
 import lightning as L
 import torch
-from torch_ema import ExponentialMovingAverage
-
 from sgmse import sampling
 from sgmse.backbones import BackboneRegistry
 from sgmse.sdes import SDERegistry
 from sgmse.util.inference import evaluate_model
 from sgmse.util.other import pad_spec
+from torch_ema import ExponentialMovingAverage
 
 
 class ScoreModel(L.LightningModule):
@@ -34,15 +33,20 @@ class ScoreModel(L.LightningModule):
         parser.add_argument(
             "--num_eval_files",
             type=int,
-            default=20,
+            default=10,
             help="Number of files for speech enhancement performance evaluation during training. Pass 0 to turn off (no checkpoints based on evaluation metrics will be generated).",
         )
         parser.add_argument(
             "--loss_type",
             type=str,
             default="mse",
-            choices=("mse", "mae"),
             help="The type of loss function to use.",
+        )
+        parser.add_argument(
+            "--loss_abs_exponent",
+            type=float,
+            default=0.5,
+            help="magnitude transformation in the loss term",
         )
         return parser
 
@@ -53,7 +57,8 @@ class ScoreModel(L.LightningModule):
         lr=1e-4,
         ema_decay=0.999,
         t_eps=3e-2,
-        num_eval_files=20,
+        loss_abs_exponent=0.5,
+        num_eval_files=10,
         loss_type="mse",
         data_module_cls=None,
         **kwargs,
@@ -74,6 +79,14 @@ class ScoreModel(L.LightningModule):
         dnn_cls = BackboneRegistry.get_by_name(backbone)
         self.dnn = dnn_cls(**kwargs)
         # Initialize SDE
+        if sde == "bbve":
+            # change parameters, if the old class bbve is used. Needed for loading the provided checkpoint
+            # as that checkpoint was trained with the old class.
+            sde = "bbed"
+            kwargs["k"] = kwargs["sigma_max"]
+            del kwargs["sigma_max"]
+            del kwargs["sigma_min"]
+
         sde_cls = SDERegistry.get_by_name(sde)
         self.sde = sde_cls(**kwargs)
         # Store hyperparams and save them
@@ -84,7 +97,7 @@ class ScoreModel(L.LightningModule):
         self.t_eps = t_eps
         self.loss_type = loss_type
         self.num_eval_files = num_eval_files
-
+        self.loss_abs_exponent = loss_abs_exponent
         self.save_hyperparameters(ignore=["no_wandb"])
         self.data_module = data_module_cls(**kwargs, gpu=kwargs.get("gpus", 0) > 0)
 
@@ -131,8 +144,9 @@ class ScoreModel(L.LightningModule):
     def eval(self, no_ema=False):
         return self.train(False, no_ema=no_ema)
 
-    def _loss(self, err):
+    def _loss(self, score, sigmas, z):
         if self.loss_type == "mse":
+            err = sigmas * score + z
             losses = torch.square(err.abs())
         elif self.loss_type == "mae":
             losses = err.abs()
@@ -143,34 +157,34 @@ class ScoreModel(L.LightningModule):
 
     def _step(self, batch, batch_idx):
         x, y = batch
-        t = (
+        rdm = (
             torch.rand(x.shape[0], device=x.device) * (self.sde.T - self.t_eps)
             + self.t_eps
         )
+        t = torch.min(rdm, torch.tensor(self.sde.T))
         mean, std = self.sde.marginal_prob(x, t, y)
-        z = torch.randn_like(x)  # i.i.d. normal distributed with var=0.5
+        z = torch.randn_like(x)  #
         sigmas = std[:, None, None, None]
         perturbed_data = mean + sigmas * z
         score = self(perturbed_data, t, y)
-        err = score * sigmas + z
-        loss = self._loss(err)
+        loss = self._loss(score, sigmas, z)
         return loss
 
     def training_step(self, batch, batch_idx):
         loss = self._step(batch, batch_idx)
-        self.log("train_loss", loss)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, sync_dist=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         loss = self._step(batch, batch_idx)
-        self.log("valid_loss", loss)
+        self.log("valid_loss", loss, on_step=False, on_epoch=True, sync_dist=True)
 
         # Evaluate speech enhancement performance
         if batch_idx == 0 and self.num_eval_files != 0:
             pesq, si_sdr, estoi = evaluate_model(self, self.num_eval_files)
-            self.log("pesq", pesq)
-            self.log("si_sdr", si_sdr)
-            self.log("estoi", estoi)
+            self.log("pesq", pesq, on_step=False, on_epoch=True, sync_dist=True)
+            self.log("si_sdr", si_sdr, on_step=False, on_epoch=True, sync_dist=True)
+            self.log("estoi", estoi, on_step=False, on_epoch=True, sync_dist=True)
 
         return loss
 
@@ -188,7 +202,15 @@ class ScoreModel(L.LightningModule):
         return super().to(*args, **kwargs)
 
     def get_pc_sampler(
-        self, predictor_name, corrector_name, y, N=None, minibatch=None, **kwargs
+        self,
+        predictor_name,
+        corrector_name,
+        y,
+        Y_prior=None,
+        N=None,
+        minibatch=None,
+        timestep_type=None,
+        **kwargs,
     ):
         N = self.sde.N if N is None else N
         sde = self.sde.copy()
@@ -197,7 +219,14 @@ class ScoreModel(L.LightningModule):
         kwargs = {"eps": self.t_eps, **kwargs}
         if minibatch is None:
             return sampling.get_pc_sampler(
-                predictor_name, corrector_name, sde=sde, score_fn=self, y=y, **kwargs
+                predictor_name,
+                corrector_name,
+                sde=sde,
+                score_fn=self,
+                Y=y,
+                Y_prior=Y_prior,
+                timestep_type=timestep_type,
+                **kwargs,
             )
         else:
             M = y.shape[0]
@@ -206,12 +235,14 @@ class ScoreModel(L.LightningModule):
                 samples, ns = [], []
                 for i in range(int(ceil(M / minibatch))):
                     y_mini = y[i * minibatch : (i + 1) * minibatch]
+                    y_prior_mini = Y_prior[i * minibatch : (i + 1) * minibatch]
                     sampler = sampling.get_pc_sampler(
                         predictor_name,
                         corrector_name,
                         sde=sde,
                         score_fn=self,
-                        y=y_mini,
+                        Y=y_mini,
+                        y_prior=y_prior_mini,
                         **kwargs,
                     )
                     sample, n = sampler()
@@ -222,14 +253,18 @@ class ScoreModel(L.LightningModule):
 
             return batched_sampling_fn
 
-    def get_ode_sampler(self, y, N=None, minibatch=None, **kwargs):
+    def get_ode_sampler(
+        self, y, Y_prior=None, N=None, minibatch=None, timestep_type=None, **kwargs
+    ):
         N = self.sde.N if N is None else N
         sde = self.sde.copy()
         sde.N = N
 
         kwargs = {"eps": self.t_eps, **kwargs}
         if minibatch is None:
-            return sampling.get_ode_sampler(sde, self, y=y, **kwargs)
+            return sampling.get_ode_sampler(
+                sde, self, y=y, Y_prior=Y_prior, timestep_type=timestep_type, **kwargs
+            )
         else:
             M = y.shape[0]
 
@@ -293,8 +328,10 @@ class ScoreModel(L.LightningModule):
         T_orig = y.size(1)
         norm_factor = y.abs().max().item()
         y = y / norm_factor
+
         Y = torch.unsqueeze(self._forward_transform(self._stft(y.cuda())), 0)
         Y = pad_spec(Y)
+
         if sampler_type == "pc":
             sampler = self.get_pc_sampler(
                 predictor,
@@ -311,15 +348,82 @@ class ScoreModel(L.LightningModule):
         else:
             print("{} is not a valid sampler type!".format(sampler_type))
         sample, nfe = sampler()
-        x_hat = self.to_audio(sample.squeeze(), T_orig)
+
+        sample = sample.squeeze()
+
+        x_hat = self.to_audio(sample, T_orig)
         x_hat = x_hat * norm_factor
-        x_hat = x_hat.cpu().numpy()
+        x_hat = x_hat.squeeze().cpu().numpy()
         end = time.time()
         if timeit:
             rtf = (end - start) / (len(x_hat) / sr)
             return x_hat, nfe, rtf
         else:
             return x_hat
+
+    def get_prior(self, y, x, n, T=1):
+        norm_factor = y.abs().max().item()
+        y = y / norm_factor
+        x = x / norm_factor
+        n = n / norm_factor
+
+        Y = torch.unsqueeze(self._forward_transform(self._stft(y.cuda())), 0)
+        Y = pad_spec(Y)
+
+        X = torch.unsqueeze(self._forward_transform(self._stft(x.cuda())), 0)
+        diff_pad = Y.shape[-1] - X.shape[-1]
+        X = pad_spec(X)
+
+        Ns = Y - X
+
+        if len(Y.shape) == 4:
+            Y = Y * self.preemp[None, None, :, None].to(device=Y.device)
+            Ns = Ns * self.preemp[None, None, :, None].to(device=Y.device)
+            X = X * self.preemp[None, None, :, None].to(device=Y.device)
+        elif len(Y.self.shape) == 3:
+            Y = Y * self.preemp[None, :, None].to(device=Y.device)
+        else:
+            Y = Y * self.preemp[:, None].to(device=Y.device)
+            X = X * self.preemp[:, None].to(device=X.device)
+            Ns = Ns * self.preemp[:, None].to(device=Ns.device)
+
+        if self.sde.__class__.__name__ == "BBVE":
+            self.sde.T = T
+        Yt, z = self.sde.prior_sampling(Y.shape, Y)
+        Yt = Yt.to(Y.device)
+        z = z.to(Y.device)
+
+        vec_t = torch.ones(Y.shape[0], device=Y.device) * torch.tensor(
+            [T], device=Y.device
+        )
+
+        grad = self(Yt, vec_t, Y, vec_t[:, None, None, None])
+        std = self.sde._std(vec_t)
+
+        mp = Yt + grad * (std**2)
+        mp_np = mp.squeeze().detach().cpu().numpy()
+
+        z = z / std
+        z_np = z.squeeze().detach().cpu().numpy()
+
+        Y_np = Y.squeeze().detach().cpu().numpy()
+        X_np = X.squeeze().detach().cpu().numpy()
+        Ns_np = Ns.squeeze().detach().cpu().numpy()
+
+        Yt_np = Yt.squeeze().detach().cpu().numpy()
+        grad_np = grad.squeeze().detach().cpu().numpy()
+
+        res = z_np + grad_np
+
+        return (
+            mp_np[:, :-diff_pad],
+            X_np[:, :-diff_pad],
+            Y_np[:, :-diff_pad],
+            res[:, :-diff_pad],
+            z_np[:, :-diff_pad],
+            grad_np[:, :-diff_pad],
+            Ns_np[:, :-diff_pad],
+        )
 
 
 class DiscriminativeModel(ScoreModel):
