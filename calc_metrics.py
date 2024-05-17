ef55c1292dc3b2c7e3123f86cc9cb5493cfcfd37
Анастасия Иванова <ivawillow.a@gmail.com>
2024-05-17 22:20:16 +0300

    added resampling in calc_metrics

--- calc_metrics.py
+++ calc_metrics.py
@@ -3,11 +3,11 @@ from glob import glob
 from os.path import join
 
 import pandas as pd
+import torchaudio.transforms as T
+from pesq import pesq
+from pystoi import stoi
 from soundfile import read
-from torchmetrics.audio import (
-    PerceptualEvaluationSpeechQuality,
-    ShortTimeObjectiveIntelligibility,
-)
+from torchaudio import load
 from tqdm import tqdm
 
 from sgmse.util.other import energy_ratios, mean_std
@@ -43,29 +43,32 @@ if __name__ == "__main__":
     }
     sr = 16000
 
-    # Evaluate standard metrics
     noisy_files = sorted(glob("{}/*.wav".format(noisy_dir)))
     for noisy_file in tqdm(noisy_files):
         filename = noisy_file.split("/")[-1]
-        x, _ = read(join(clean_dir, filename))
-        y, _ = read(noisy_file)
+
+        x, sr1 = load(join(clean_dir, filename))
+        y, sr2 = load(noisy_file)
+
+        if sr1 != 16000:
+            resampler = T.Resample(orig_freq=sr1, new_freq=16000)
+            y = resampler(y).squeeze().detach().cpu().numpy()
+        if sr2 != 16000:
+            resampler = T.Resample(orig_freq=sr2, new_freq=16000)
+            x = resampler(x).squeeze().detach().cpu().numpy()
+
         n = y - x
         x_method, _ = read(join(enhanced_dir, filename))
 
-        wb_pesq = PerceptualEvaluationSpeechQuality(sr, "wb")
-        stoi = ShortTimeObjectiveIntelligibility(sr, True)
-
         data["filename"].append(filename)
-        data["pesq"].append(wb_pesq(x, x_method))
-        data["estoi"].append(stoi(x, x_method))
+        data["pesq"].append(pesq(sr, x, x_method, "wb"))
+        data["estoi"].append(stoi(x, x_method, sr, extended=True))
         data["si_sdr"].append(energy_ratios(x_method, x, n)[0])
         data["si_sir"].append(energy_ratios(x_method, x, n)[1])
         data["si_sar"].append(energy_ratios(x_method, x, n)[2])
 
-    # Save results as DataFrame
     df = pd.DataFrame(data)
 
-    # Print results
     print(enhanced_dir)
     print("PESQ: {:.2f} ± {:.2f}".format(*mean_std(df["pesq"].to_numpy())))
     print("ESTOI: {:.2f} ± {:.2f}".format(*mean_std(df["estoi"].to_numpy())))
@@ -73,5 +76,4 @@ if __name__ == "__main__":
     print("SI-SIR: {:.1f} ± {:.1f}".format(*mean_std(df["si_sir"].to_numpy())))
     print("SI-SAR: {:.1f} ± {:.1f}".format(*mean_std(df["si_sar"].to_numpy())))
 
-    # Save DataFrame as csv file
     df.to_csv(join(enhanced_dir, "_results.csv"), index=False)
